2c1e9b27c16698acfb3020e471dd00a2375a4309
gb88 <gb8253@gmail.com>
2019-11-18 14:43:52 +0100

    Add files via upload

--- ble.js
+++ ble.js
@@ -83,7 +83,6 @@ function connect() {
         })
         .then(function(device) {
             // save the device returned so you can disconnect later:
-				
             myDevice = device;
             console.log(device);
             // connect to the device once you find it:
@@ -116,9 +115,14 @@ function connect() {
             buffer[0] = 0xE1; //event count cmd
             buffer[1] = 0x00;
             buffer[2] = 0x03; //wait for 3 byte
+				waited_data = 2;
+				received_data = 0;
+				buf = new Uint8Array()
             state = 1;
             console.log(buffer)
+				delay(500);
             characteristic.writeValue(buffer);
+				delay(10);
         })
         .catch(function(error) {
             // catch any errors:
@@ -131,177 +135,236 @@ function subscribeToChanges(characteristic) {
     console.log("subscribe")
     characteristic.oncharacteristicvaluechanged = handleData;
 }
-
-
+function delay(ms) {
+   ms += new Date().getTime();
+   while (new Date() < ms){}
+}
 // handle incoming data:
 function handleData(event) {
     // get the data buffer from the meter:
     console.log("get data")
     console.log(event)
     console.log(event_num)
-    var buf = new Uint8Array(event.target.value.buffer);
+	 //window.alert(event.target.value.byteLength);
+	 if(received_data < waited_data)
+	 {
+		var tmp = new Uint8Array(received_data + event.target.value.byteLength);
+		if(received_data != 0)
+		{
+			tmp.set(buf, 0);
+			tmp.set(new Uint8Array(event.target.value.buffer), received_data);
+		}
+		else
+		{
+			tmp.set(new Uint8Array(event.target.value.buffer), 0);
+		}
+		buf = tmp;
+		//buf = new Uint8Array(event.target.value.buffer,buf);
+		received_data+=event.target.value.byteLength;
+	 }
+	 console.log("Stato")
+	 console.log(state)
+	 console.log(received_data)
+	 console.log(waited_data)
+	 console.log("Buffer")
     console.log(buf);
-    if (state == 1) {
-        console.log("First Request Answered")
-        //TODO: check della risposta
-        var buffer = new Uint8Array(3);
-        buffer[0] = 0xE1; //event count cmd
-        buffer[1] = 0x08;
-        buffer[2] = 0x7F; //crc da calcolare
-        state = 2;
-        characteristic_obj.writeValue(buffer);
-    } else if (state == 2) {
-        //risposta conto eventi
-        /*
-        1 0xE1
-        2 event num
-        1 max event for requ
-        2 crc
-        */
-        //todo gestire caso eventi nulli
-        var el = document.getElementById("list");
-        el.innerHTML = '';
-        progressive = 0;
-        var node = document.createElement("li");
-        node.setAttribute('class', 'table-header');
-        var prog = document.createElement("div");
-        prog.setAttribute('class', 'col col-1');
-        prog.innerText = "#";
-        node.appendChild(prog);
-        var number = document.createElement("div");
-        number.setAttribute('class', 'col col-2');
-        number.innerText = "Number";
-        node.appendChild(number);
-        var type = document.createElement("div");
-        type.setAttribute('class', 'col col-3');
-        type.innerText = "Type";
-        node.appendChild(type);
-        var date = document.createElement("div");
-        date.setAttribute('class', 'col col-4');
-        date.innerText = "Date";
-        node.appendChild(date);
-        var current = document.createElement("div");
-        current.setAttribute('class', 'col col-5');
-        current.innerText = "Ig";
-        node.appendChild(current);
-        el.appendChild(node);
+	 if(received_data == waited_data)
+	 {
+		 if (state == 1) {
+			  console.log("First Request Answered")
+			  //TODO: check della risposta
+			  var buffer = new Uint8Array(3);
+			  buffer[0] = 0xE1; //event count cmd
+			  buffer[1] = 0x08;
+			  buffer[2] = 0x7F; //crc da calcolare
+			  waited_data = 6;
+			  received_data = 0;
+			  buf = new Uint8Array()
+			  state = 2;
+			  characteristic_obj.writeValue(buffer);
+			  delay(10);
+		 } else if (state == 2) {
+			  //risposta conto eventi
+			  /*
+			  1 0xE1
+			  2 event num
+			  1 max event for requ
+			  2 crc
+			  */
+			  //todo gestire caso eventi nulli
+			  var el = document.getElementById("list");
+			  el.innerHTML = '';
+			  progressive = 0;
+			  var node = document.createElement("li");
+			  node.setAttribute('class', 'table-header');
+			  var prog = document.createElement("div");
+			  prog.setAttribute('class', 'col col-1');
+			  prog.innerText = "#";
+			  node.appendChild(prog);
+			  var number = document.createElement("div");
+			  number.setAttribute('class', 'col col-2');
+			  number.innerText = "Number";
+			  node.appendChild(number);
+			  var type = document.createElement("div");
+			  type.setAttribute('class', 'col col-3');
+			  type.innerText = "Type";
+			  node.appendChild(type);
+			  var date = document.createElement("div");
+			  date.setAttribute('class', 'col col-4');
+			  date.innerText = "Date";
+			  node.appendChild(date);
+			  var current = document.createElement("div");
+			  current.setAttribute('class', 'col col-5');
+			  current.innerText = "Ig";
+			  node.appendChild(current);
+			  el.appendChild(node);
 
-        event_num = buf[2];
-        event_cnt = 1;
-        total_evt = event_num;
-        //document.getElementById("event_count").innerHTML = buf[2].toString();
-        var buffer = new Uint8Array(3);
-        buffer[0] = 0xE2; //event count cmd
-        buffer[1] = 0x00;
-        buffer[2] = 0x07; //crc da calcolare
-        state = 3;
-        asked_evt = true;
-        characteristic_obj.writeValue(buffer);
-    } else if (state == 3) {
-        if (event_num > 1) {
-            var buffer = new Uint8Array(7);
-            buffer[0] = 0xE2; //chiedo 2 eventi
-            buffer[1] = event_cnt >> 8;
-            buffer[2] = event_cnt;
-            buffer[3] = (event_cnt + 1) >> 8;
-            buffer[4] = event_cnt + 1;
-            var result = CalcCrc(buffer, 5);
-            buffer[5] = result[0]; //crc da calcolare
-            buffer[6] = result[1]; //crc da calcolare
-            state = 4;
-            asked_evt_num = 2;
-            event_cnt += 2;
-            event_num -= 2;
-            characteristic_obj.writeValue(buffer);
-        } else if (event_num == 1) {
-            var buffer = new Uint8Array(7);
-            buffer[0] = 0xE2; //chiedo 1 eventi
-            buffer[1] = event_cnt >> 8;
-            buffer[2] = event_cnt;
-            buffer[3] = (event_cnt) >> 8;
-            buffer[4] = event_cnt;
-            var result = CalcCrc(buffer, 5);
-            buffer[5] = result[0]; //crc da calcolare
-            buffer[6] = result[1]; //crc da calcolare
-            state = 4;
-            asked_evt_num = 1;
-            event_cnt += 1;
-            event_num -= 1;
-            characteristic_obj.writeValue(buffer);
-        }
+			  event_num = buf[2];
+			  event_cnt = 1;
+			  total_evt = event_num;
+			  //document.getElementById("event_count").innerHTML = buf[2].toString();
+			  var buffer = new Uint8Array(3);
+			  buffer[0] = 0xE2; //event count cmd
+			  buffer[1] = 0x00;
+			  buffer[2] = 0x07; //crc da calcolare
+			  waited_data = 2;
+			  received_data = 0;
+			  buf = new Uint8Array()
+			  state = 3;
+			  asked_evt = true;
+			  characteristic_obj.writeValue(buffer);
+			  delay(10);
+		 } else if (state == 3) {
+			  if (event_num > 1) {
+					var buffer = new Uint8Array(7);
+					buffer[0] = 0xE2; //chiedo 2 eventi
+					buffer[1] = event_cnt >> 8;
+					buffer[2] = event_cnt;
+					buffer[3] = (event_cnt + 1) >> 8;
+					buffer[4] = event_cnt + 1;
+					var result = CalcCrc(buffer, 5);
+					buffer[5] = result[0]; //crc da calcolare
+					buffer[6] = result[1]; //crc da calcolare
+					state = 4;
+					asked_evt_num = 2;
+					event_cnt += 2;
+					event_num -= 2;
+					waited_data = 41;
+			      received_data = 0;
+					buf = new Uint8Array()
+					characteristic_obj.writeValue(buffer);
+					delay(10);
+			  } else if (event_num == 1) {
+					var buffer = new Uint8Array(7);
+					buffer[0] = 0xE2; //chiedo 1 eventi
+					buffer[1] = event_cnt >> 8;
+					buffer[2] = event_cnt;
+					buffer[3] = (event_cnt) >> 8;
+					buffer[4] = event_cnt;
+					var result = CalcCrc(buffer, 5);
+					buffer[5] = result[0]; //crc da calcolare
+					buffer[6] = result[1]; //crc da calcolare
+					state = 4;
+					asked_evt_num = 1;
+					event_cnt += 1;
+					event_num -= 1;
+					waited_data = 25;
+			      received_data = 0;
+					buf = new Uint8Array()
+					characteristic_obj.writeValue(buffer);
+					delay(10);
+			  }
 
-    } else if (state == 4) {
-        move(Math.round((event_cnt - 1) / total_evt * 100))
-        var step = 0;
-        if (asked_evt == true) {
-            for (i = 0; i < asked_evt_num; i++) {
-                progressive++;
-                console.log("decode_evt")
-                var number = (buf[7 + step] + buf[8 + step] * 256).toString();
-                var type = (buf[9 + step] + buf[10 + step] * 256); //.toString();
-                var date = Unix_timestamp(buf[11 + step] + buf[12 + step] * 256 + buf[13 + step] * 256 * 256 + buf[14 + step] * 256 * 256 * 256, (buf[15 + step] + buf[16 + step] * 256).toString());
-                var current = ((buf[19 + step] + buf[20 + step] * 256 + buf[21 + step] * 256 * 256 + buf[22 + step] * 256 * 256 * 256) / 100).toString();
-                var el = document.getElementById("list");
-                var node = document.createElement("li");
-                node.setAttribute('class', 'table-row');
+		 } else if (state == 4) {
+			  move(Math.round((event_cnt - 1) / total_evt * 100))
+			  var step = 0;
+			  if (asked_evt == true) {
+					for (i = 0; i < asked_evt_num; i++) {
+						 progressive++;
+						 console.log("decode_evt")
+						 var number = (buf[7 + step] + buf[8 + step] * 256).toString();
+						 var type = (buf[9 + step] + buf[10 + step] * 256); //.toString();
+						 var date = Unix_timestamp(buf[11 + step] + buf[12 + step] * 256 + buf[13 + step] * 256 * 256 + buf[14 + step] * 256 * 256 * 256, (buf[15 + step] + buf[16 + step] * 256).toString());
+						 var current = ((buf[19 + step] + buf[20 + step] * 256 + buf[21 + step] * 256 * 256 + buf[22 + step] * 256 * 256 * 256) / 100).toString();
+						 var el = document.getElementById("list");
+						 var node = document.createElement("li");
+						 node.setAttribute('class', 'table-row');
 
-                var col0 = document.createElement("div");
-                col0.setAttribute('class', 'col col-1');
-                col0.setAttribute('data-label', '#');
-                col0.innerText = progressive;
-                node.appendChild(col0);
+						 var col0 = document.createElement("div");
+						 col0.setAttribute('class', 'col col-1');
+						 col0.setAttribute('data-label', '#');
+						 col0.innerText = progressive;
+						 node.appendChild(col0);
 
-                var col1 = document.createElement("div");
-                col1.setAttribute('class', 'col col-2');
-                col1.setAttribute('data-label', 'Number');
-                col1.innerText = number;
-                node.appendChild(col1);
+						 var col1 = document.createElement("div");
+						 col1.setAttribute('class', 'col col-2');
+						 col1.setAttribute('data-label', 'Number');
+						 col1.innerText = number;
+						 node.appendChild(col1);
 
-                var col2 = document.createElement("div");
-                col2.setAttribute('class', 'col col-3');
-                col2.setAttribute('data-label', 'Type');
-                col2.innerText = event_list[type - 1];
-                node.appendChild(col2);
+						 var col2 = document.createElement("div");
+						 col2.setAttribute('class', 'col col-3');
+						 col2.setAttribute('data-label', 'Type');
+						 col2.innerText = event_list[type - 1];
+						 node.appendChild(col2);
 
-                var col3 = document.createElement("div");
-                col3.setAttribute('class', 'col col-4');
-                col3.setAttribute('data-label', 'Date');
-                col3.innerText = date;
-                node.appendChild(col3);
+						 var col3 = document.createElement("div");
+						 col3.setAttribute('class', 'col col-4');
+						 col3.setAttribute('data-label', 'Date');
+						 col3.innerText = date;
+						 node.appendChild(col3);
 
-                var col4 = document.createElement("div");
-                col4.setAttribute('class', 'col col-5');
-                col4.setAttribute('data-label', 'Ig');
-                col4.innerText = current;
-                node.appendChild(col4);
+						 var col4 = document.createElement("div");
+						 col4.setAttribute('class', 'col col-5');
+						 col4.setAttribute('data-label', 'Ig');
+						 col4.innerText = current;
+						 node.appendChild(col4);
 
 
-                el.appendChild(node);
-                step += 16;
-            }
-        }
-        //risposta conto eventi
-        /*
-        1 0xE1
-        2 event num
-        1 max event for requ
-        2 crc
-        */
-        if (event_num != 0) {
-            var buffer = new Uint8Array(3);
-            buffer[0] = 0xE2; //event count cmd
-            buffer[1] = 0x00;
-            buffer[2] = 0x07; //crc da calcolare
-            state = 3;
-            characteristic_obj.writeValue(buffer);
-        } else {
-            console.log("Disconnected")
-            //disconnect
-            asked_evt = false;
-            disconnect();
-        }
-    }
+						 el.appendChild(node);
+						 step += 16;
+					}
+			  }
+			  //risposta conto eventi
+			  /*
+			  1 0xE1
+			  2 event num
+			  1 max event for requ
+			  2 crc
+			  */
+			  if (event_num != 0) {
+					var buffer = new Uint8Array(3);
+					buffer[0] = 0xE2; //event count cmd
+					buffer[1] = 0x00;
+					buffer[2] = 0x07; //crc da calcolare
+					state = 3;
+					waited_data = 2;
+			      received_data = 0;
+					buf = new Uint8Array()
+					characteristic_obj.writeValue(buffer);
+					delay(10);
+			  } else {
+					console.log("Disconnected")
+					//disconnect
+					asked_evt = false;
+					disconnect();
+			  }
+		 }
+	 }
 }
+function concatArrayBuffers(buffer1, buffer2) {
+
+  if (!buffer1) {
+    return buffer2;
+  } else if (!buffer2) {
+    return buffer1;
+  }
+
+  var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+  tmp.set(new Uint8Array(buffer1), 0);
+  tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+  return tmp.buffer;
+};
 
 // disconnect function:
 function disconnect() {
